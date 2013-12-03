0839f0fbe7aef885e1410b6253238b9a698a90a9
zs1621 <zs1213yh@gmail.com>
2013-12-03 11:28:55 +0800

    add some comments about connect_pool_mysql

--- mysql-connect/connect_pool_mysql.js
+++ mysql-connect/connect_pool_mysql.js
@@ -1,5 +1,16 @@
+/**
+ * test generic-pool reconnect  
+ * 1. node connect_pool_mysql.js 
+ * 2. curl 127.0.0.1:1337
+ * 3. mysql -uuser -ppassword
+ * 4. set wait_timeout=10
+ * 5. wait 10 sencods
+ * 6. curl 127.0.0.1:1337
+ * 7. see mysql log 
+ **/
 var mysql = require('mysql')
 var generic_pool = require('generic-pool');
+var http = require('http');
 
 var pool = generic_pool.Pool({
     name: 'mysql',
@@ -22,14 +33,17 @@ var pool = generic_pool.Pool({
     log: true
 });
 
-for (var i=0; i < 10000; i++) {
-pool.acquire(function(err, client){
+var app = http.createServer(function (req, res) {
+    pool.acquire(function(err, client){
         if (err) {
         } else {
             client.query("select * from t_user", [], function(err, data){
-                console.log(data); 
+                res.writeHead(200, {'Content-Type': 'text/plain'});; 
+                res.end(String(data))
                 pool.release(client);
             }); 
         }
+    });
 });
-}
+
+app.listen(1337);
